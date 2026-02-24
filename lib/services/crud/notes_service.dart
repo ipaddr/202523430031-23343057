04ccf4514e6ccc9d84859dc278d7044444ce563a
SirGhazian <ghazianunp99@gmail.com>
2026-02-25 01:26:59 +0700

    CRUD local storage

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -52,5 +52,6 @@ class DatabaseNote {
 const idColumn = 'id';
 const emailColumn = 'email';
 const userIdColumn = 'user_id';
+
 const textColumn = 'text';
 const isSyncedWithCloudColumn = 'is_synced_with_cloud';
