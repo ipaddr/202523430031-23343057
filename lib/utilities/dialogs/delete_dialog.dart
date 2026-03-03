e5de908ad4d973fdaaaa998ccfa97597921233f3
SirGhazian <ghazianunp99@gmail.com>
2026-03-03 22:59:04 +0700

    add dialog files

---
+++ lib/utilities/dialogs/delete_dialog.dart
@@ -0,0 +1,11 @@
+import 'package:mynotes/utilities/dialogs/generic_dialog.dart';
+import 'package:flutter/material.dart';
+
+Future<bool> showDeleteDialog(BuildContext context) {
+  return showGenericDialog<bool>(
+    context: context,
+    title: "Delete",
+    content: "Are you sure you want to delete this note?",
+    optionsBuilder: () => {"Cancel": false, "Yes": true},
+  ).then((value) => value ?? false);
+}
---
+++ lib/utilities/dialogs/logout_dialog.dart
@@ -0,0 +1,11 @@
+import 'package:mynotes/utilities/dialogs/generic_dialog.dart';
+import 'package:flutter/material.dart';
+
+Future<bool> showlogoutDialog(BuildContext context) {
+  return showGenericDialog<bool>(
+    context: context,
+    title: "Log out",
+    content: "Are you sure you want to sign out?",
+    optionsBuilder: () => {"Cancel": false, "Log out": true},
+  ).then((value) => value ?? false);
+}
