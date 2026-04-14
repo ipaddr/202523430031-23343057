eff8de85fe6a54f5da6bc2dd2ce401fc67f96496
SirGhazian <ghazianunp99@gmail.com>
2026-04-15 01:16:23 +0700

    simpan data ke lokal

--- lib/models/student.dart
+++ lib/models/student.dart
@@ -10,4 +10,18 @@ class Student {
     required this.nim,
     this.attendanceCount = 0,
   });
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'nim': nim,
+        'attendanceCount': attendanceCount,
+      };
+
+  factory Student.fromJson(Map<String, dynamic> json) => Student(
+        id: json['id'],
+        name: json['name'],
+        nim: json['nim'],
+        attendanceCount: json['attendanceCount'] ?? 0,
+      );
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../models/student.dart';
 import 'paste_attendance_screen.dart';
 import 'student_list_screen.dart';
@@ -13,21 +15,58 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int _currentIndex = 0;
   List<Student> students = [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? studentsData = prefs.getString('students');
+    
+    if (studentsData != null) {
+      final List<dynamic> decoded = jsonDecode(studentsData);
+      setState(() {
+        students = decoded.map((item) => Student.fromJson(item)).toList();
+      });
+    }
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  Future<void> _saveData() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String encoded = jsonEncode(students.map((e) => e.toJson()).toList());
+    await prefs.setString('students', encoded);
+  }
 
   void _addStudent(Student student) {
     setState(() {
       students.add(student);
     });
+    _saveData();
   }
 
   void _updateStudents(List<Student> updatedList) {
     setState(() {
       students = updatedList;
     });
+    _saveData();
   }
 
   @override
   Widget build(BuildContext context) {
+    if (_isLoading) {
+      return const Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(child: CircularProgressIndicator(color: Colors.black)),
+      );
+    }
+
     final List<Widget> screens = [
       StudentListScreen(
         students: students,
